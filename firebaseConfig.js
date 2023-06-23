380f15301846c0c325442ad35046f2bdf31c4e2e
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-23 10:02:28 +0200

    Added fIREBASE

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAeI6dCmI8blXfCrw-HmN9XVjFaQrSLffg",
+  authDomain: "react-native-570ed.firebaseapp.com",
+  projectId: "react-native-570ed",
+  storageBucket: "react-native-570ed.appspot.com",
+  messagingSenderId: "739537378950",
+  appId: "1:739537378950:web:3a0fdd61aa8af3bdc03b07",
+  measurementId: "G-VD3GEHQ3BC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
